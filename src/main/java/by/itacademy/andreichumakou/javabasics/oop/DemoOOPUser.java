2eaf36f97c6d45be2029111813c30c2a8bae9b03
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-10 15:12:22 +0300

    refactor DemoOOPUser.java

--- src/main/java/by/itacademy/andreichumakou/javabasics/oop/DemoOOPUser.java
+++ src/main/java/by/itacademy/andreichumakou/javabasics/oop/DemoOOPUser.java
@@ -1,10 +1,32 @@
 package by.itacademy.andreichumakou.javabasics.oop;
 
+import java.util.ArrayList;
+
 public class DemoOOPUser {
     public static void main(String[] args) {
-        Student student = new Student(100);
-        student.setName("Ivan");
+        Person person = new Person(100, 400);
+        person.setName("Ivanoff");
+        System.out.println(person.getIncome());
+
+        Student student = new Person(100, 400);
+        student.setName("Sidoroff");
         System.out.println(student.getIncome());
+
+        User user = new Person(100, 400);
+        user.setName("Petroff");
+        System.out.println( ((Person)user).getIncome());
+
+        ArrayList<User> users = new ArrayList<>();
+        users.add(person);
+        users.add(student);
+        users.add(user);
+
+        for (User element:users) {
+            if (element instanceof Person) {
+                System.out.println(element.getName() + " has " + ((Person) element).getIncome());
+            }
+        }
+
 
     }
 }
