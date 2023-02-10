90b36c9d9e66b4b1f68b7b3cc66a1a56068568b9
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-10 12:50:19 +0300

    refactor DemoOOP.java add using Book class

--- src/main/java/by/itacademy/andreichumakou/javabasics/oop/DemoOOP.java
+++ src/main/java/by/itacademy/andreichumakou/javabasics/oop/DemoOOP.java
@@ -11,6 +11,38 @@ public class DemoOOP {
         customer001.setNumberCreditCard(456274123694581L);
         System.out.println(customer001);
         System.out.println("ID: " + customer001.getId());
+
+
+        Book book001 = new Book();
+        book001.setTitle("Метро 2033");
+        book001.setAuthors("Дмитрий Глуховский");
+        book001.setPublishingHouse("АСТ");
+        book001.setQuantityOfPages(384);
+        book001.setPrice(35.36);
+        book001.setId(10796109);
+        book001.setYearOfPublishing(2019);
+        System.out.println(book001);
+
+        Book book002 = new Book();
+        book002.setTitle("Дюна");
+        book002.setAuthors("Фрэнк Герберт");
+        book002.setPublishingHouse("АСТ");
+        book002.setQuantityOfPages(704);
+        book002.setPrice(29.27);
+        book002.setId(10916281);
+        book002.setYearOfPublishing(2020);
+        System.out.println(book002);
+
+        Book book003 = new Book();
+        book003.setTitle("Позже");
+        book003.setAuthors("Стивен Кинг");
+        book003.setPublishingHouse("");
+        book003.setQuantityOfPages(-500);
+        book003.setPrice(19.77);
+        book003.setId(101040785);
+        book003.setYearOfPublishing(2021);
+        System.out.println(book003);
+
     }
 
 }
