850725c5d176dfc5ba0e8c15cdbff44f93122b5a
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-10 14:47:50 +0300

    add DemoOOPUser.java

---
+++ src/main/java/by/itacademy/andreichumakou/javabasics/oop/DemoOOPUser.java
@@ -0,0 +1,10 @@
+package by.itacademy.andreichumakou.javabasics.oop;
+
+public class DemoOOPUser {
+    public static void main(String[] args) {
+        Student student = new Student(100);
+        student.setName("Ivan");
+        System.out.println(student.getIncome());
+
+    }
+}
