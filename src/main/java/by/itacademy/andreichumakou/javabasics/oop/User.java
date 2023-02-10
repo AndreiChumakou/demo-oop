a7860e538ced5d3b128f75daf69cb0ce9cba8eb3
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-10 14:01:17 +0300

    add User.java

---
+++ src/main/java/by/itacademy/andreichumakou/javabasics/oop/User.java
@@ -0,0 +1,16 @@
+package by.itacademy.andreichumakou.javabasics.oop;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+}
